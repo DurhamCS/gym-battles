5ba654149db5227edc33a3badd7cab7560d091a5
RGBPlaza <rayred14@live.com>
2020-11-14 18:00:35 +0000

    some transition stuff

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { ReactChildren } from "react";
+import { CSSTransition } from "react-transition-group";
+import { initializeIcons } from "@uifabric/icons";
+import { Icon } from "@fluentui/react/lib/Icon";
+
+initializeIcons();
 
 interface btnProps {
   idx: number;
   selectedIdx: number;
   text: string;
   hoverHandler: (idx: number) => void;
+  clickHandler: (idx: number) => void;
+}
+
+interface slideProps {
+  closeHandler: VoidFunction;
+  children: unknown;
+  className: string;
 }
 
 function Btn(props: btnProps) {
@@ -22,7 +34,11 @@ function Btn(props: btnProps) {
             d="M 0,0 L 0,24 20,12 Z"
           />
         </svg>
-        <p className="HomePage-btn HomePage-btn-selected">{props.text}</p>
+        <p
+          className="HomePage-btn HomePage-btn-selected"
+          onClick={() => props.clickHandler(props.idx)}>
+          {props.text}
+        </p>
       </div>
     );
   else
@@ -37,20 +53,73 @@ function Btn(props: btnProps) {
 
 const buttons = ["Create Session", "Join Session"];
 
-export class HomePage extends React.Component<{}, { btnIdx: number }, any> {
+function Slide(props: slideProps) {
+  return (
+    <div className={"HomePage-slide " + props.className}>
+      <Icon
+        onClick={() => props.closeHandler()}
+        iconName="Back"
+        className="HomePage-slide-back-button"
+      />
+      {props.children}
+    </div>
+  );
+}
+
+interface IHomePageState {
+  btnIdx: number;
+  isJoining: boolean;
+  isCreating: boolean;
+  hasCreated: boolean;
+}
+
+export class HomePage extends React.Component<{}, IHomePageState, any> {
   constructor(props: any) {
     super(props);
-    this.state = { btnIdx: 0 };
+    this.state = {
+      btnIdx: 0,
+      isJoining: false,
+      isCreating: false,
+      hasCreated: false,
+    };
     document.onkeydown = ev => {
-      if (ev.keyCode === 38)
-        this.setState({ btnIdx: (this.state.btnIdx - 1) % buttons.length });
-      else if (ev.keyCode === 40)
-        this.setState({ btnIdx: (this.state.btnIdx + 1) % buttons.length });
+      if (
+        !this.state.isJoining &&
+        !this.state.isCreating &&
+        !this.state.hasCreated
+      ) {
+        switch (ev.key) {
+          case "ArrowUp":
+            this.setState({ btnIdx: (this.state.btnIdx - 1) % buttons.length });
+            break;
+          case "ArrowDown":
+            this.setState({ btnIdx: (this.state.btnIdx + 1) % buttons.length });
+            break;
+          case "Enter":
+            this.handleClick(this.state.btnIdx);
+            break;
+        }
+      }
     };
   }
 
   handleHover = (idx: number) => {
     this.setState({ btnIdx: idx });
+  };
+
+  handleClick = (idx: number) => {
+    switch (idx) {
+      case 0:
+        this.setState({ isCreating: true, isJoining: false });
+        break;
+      case 1:
+        this.setState({ isCreating: false, isJoining: true });
+        break;
+    }
+  };
+
+  closeSlide = () => {
+    this.setState({ isCreating: false, isJoining: false });
   };
 
   render() {
@@ -63,6 +132,7 @@ export class HomePage extends React.Component<{}, { btnIdx: number }, any> {
             idx={i}
             selectedIdx={this.state.btnIdx}
             hoverHandler={this.handleHover}
+            clickHandler={this.handleClick}
           />
         ))}
         <svg className="HomePage-bottom-svg" viewBox="0 0 100 50">
@@ -74,6 +144,18 @@ export class HomePage extends React.Component<{}, { btnIdx: number }, any> {
           </defs>
           <path d="M 0,50 L 100,50 100,35 Z" fill="url(#grad1)" />
         </svg>
+
+        <CSSTransition
+          in={this.state.isCreating}
+          timeout={250}
+          classNames="HomePage-slide">
+          <Slide
+            closeHandler={this.closeSlide}
+            className="HomePage-builder-slide">
+            <p></p>
+          </Slide>
+        </CSSTransition>
+
         <svg className="HomePage-bottom-svg" viewBox="0 0 100 50">
           <defs>
             <linearGradient id="grad2" x1="0%" y1="100%" x2="100%" y2="0%">
@@ -83,6 +165,15 @@ export class HomePage extends React.Component<{}, { btnIdx: number }, any> {
           </defs>
           <path d="M 0,50 L 100,50 100,40 Z" fill="url(#grad2)" />
         </svg>
+
+        <CSSTransition
+          in={this.state.isJoining || this.state.hasCreated}
+          timeout={250}
+          classNames="HomePage-slide">
+          <Slide closeHandler={this.closeSlide} className="HomePage-code-slide">
+            <p></p>
+          </Slide>
+        </CSSTransition>
       </header>
     );
   }
