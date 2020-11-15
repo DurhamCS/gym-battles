93e90141800058cac197b23988f5058982cae89e
Crambor <dariuscpt@gmail.com>
2020-11-15 06:21:12 +0000

    add get reps and hope it works

---
+++ model.js
@@ -0,0 +1,4 @@
+// this function needs to use the model, format it to one hot and return
+export function predict(data) {
+    return undefined;
+}
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const server = http.createServer(app)
 const socket = require('socket.io')
 const io = socket(server)
 const username = require('username-generator')
+const model = require('model')
 
 app.use(express.static('./client/build'));
 
@@ -12,13 +13,44 @@ app.get('*', (req,res)=>{
     res.sendFile(path.resolve(__dirname, "client","build","index.html"));
 })
 
-const users={}
+// TODO: put all of these in one object
+const reps = {}
+const users = {}
+const user_motion = {}
+
+
+function get_num_reps(position, userid) {
+    if (position !== user_motion[userid]) {
+        if (position === [1, 0, 0]) {
+            user_motion[userid] = [0, 0, 1]
+            reps[userid] += 0.5
+        }
+        else if (position === [0, 0, 1]) {
+            user_motion[userid] = [1, 0, 0]
+            reps[userid] += 0.5
+        }
+    }
+
+    return Math.floor(reps[userid])
+}
+
+
+app.get('/getReps', function (req, res) {
+    // get this from req somehow
+    let data = {userid: null, probabilities: null}
+    let model_data = model.predict(data.probabilities)
+    let num_reps = get_num_reps(model_data, data.userid)
+    res.end(num_reps)
+})
+
 
 io.on('connection', socket => {
     //generate username against a socket connection and store it
     const userid=username.generateUsername('-')
     if(!users[userid]){
         users[userid] = socket.id
+        reps[userid] = 0
+        user_motion[userid] = [1, 0, 0]  // assuming the user starts at bottom
     }
     //send back username
     socket.emit('yourID', userid)
@@ -26,6 +58,8 @@ io.on('connection', socket => {
     
     socket.on('disconnect', ()=>{
         delete users[userid]
+        delete reps[userid]
+        delete user_motion[userid]
     })
 
     socket.on('callUser', (data)=>{
